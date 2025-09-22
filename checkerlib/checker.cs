a72ae0ea7abe4a85995e01980fec9e4322b11b6e
haripriya-pr <prharipriya1998@gmail.com>
2025-09-22 12:00:45 +0530

    Extend 3 & 4 - Dev completed

--- checkerlib/checker.cs
+++ checkerlib/checker.cs
@@ -4,6 +4,8 @@ using Castle.Core.Internal;
 using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Text.RegularExpressions;
+
 
 public interface ICheckerDisplay
 {
@@ -25,9 +27,31 @@ public class ConsoleDisplay : ICheckerDisplay
     }
 }
 
+
 public class Vitals
 {
-    public float Temperature { get; set; }
+    private string _temperatureString { get; set; } = string.Empty;
+    private float _temperatureValue { get; set; }
+    Regex tempRegex = new Regex(@"^\d+(\.\d+)?(C|F)$", RegexOptions.IgnoreCase);
+    //Temperature in F by default
+    public string Temperature { get { return _temperatureValue.ToString(); } 
+        set {
+            if (tempRegex.IsMatch(value) && !_temperatureString.Equals(value,StringComparison.OrdinalIgnoreCase))
+            {
+                string numericPart = value.Substring(0, value.Length - 2);
+                if(float.TryParse(numericPart,out float parsedValue))
+                {
+                    if(value.LastOrDefault() == 'C')
+                    {
+                        parsedValue = (parsedValue * 9 / 5) + 32;
+                    }
+                    _temperatureValue = parsedValue;
+                    _temperatureString = value;
+                }
+            }
+        }
+    }
+
     public int PulseRate {  get; set; }
     public int? OxygenSaturation { get; set; }
     public int BloodSugar { get; set; }
@@ -42,14 +66,21 @@ public class VitalLimits
     public float? VitalMinimum { get; set; }
 }
 
+public class VitalLevels
+{
+    public string? Low { get; set; }
+    public string? High { get; set; }
+    public string? Normal { get; set; }
+}
+
 public class VitalLanguage
 {
-    public string Temperature { get; set; }
-    public string PulseRate { get; set; }
-    public string OxygenSaturation { get; set; }
-    public string BloodSugar { get; set; }
-    public string BloodPressure { get; set; }
-    public string RespiratoryRate { get; set; }
+    public VitalLevels Temperature { get; set; } = new() { Low = "Hypothermia",High = "Hyperthermia", Normal = "Temperature" };
+    public VitalLevels PulseRate { get; set; } = new() { Low = "Bradycardia", High = "Tachycardia", Normal = "Pulse rate" };
+    public VitalLevels OxygenSaturation { get; set; } = new() { Low = "Hypoxemia", High = "Hyperoxia", Normal = "Oxygen saturation" };
+    public VitalLevels BloodSugar { get; set; } = new() { Low = "Hypoglycemia", High = "Hyperglycemia", Normal = "Blood sugar" };
+    public VitalLevels BloodPressure { get; set; } = new() { Low = "Hypotension", High = "Hypertension", Normal = "Blood pressure" };
+    public VitalLevels RespiratoryRate { get; set; } = new() { Low = "Bradypnea", High = "Tachypnea", Normal = "Respiratory rate" };
 }
 
 public class Checker (ICheckerDisplay display)
@@ -58,7 +89,7 @@ public class Checker (ICheckerDisplay display)
     private readonly ICheckerDisplay _display = display;
     private readonly Vitals lowerLimit = new()
     {
-        Temperature = 95,
+        Temperature = "95",
         PulseRate = 60,
         OxygenSaturation = 90,
         BloodSugar = 70,
@@ -67,7 +98,7 @@ public class Checker (ICheckerDisplay display)
     };
     private readonly Vitals upperLimit = new()
     {
-        Temperature = 102,
+        Temperature = "102",
         PulseRate = 100,
         OxygenSaturation = null,
         BloodSugar = 110,
@@ -76,13 +107,14 @@ public class Checker (ICheckerDisplay display)
     };
     private readonly VitalLanguage german = new()
     {
-        Temperature = "Temperatur",
-        PulseRate = "Pulsfrequenz",
-        OxygenSaturation = "Sauerstoffsättigung",
-        BloodSugar = "Blutzucker",
-        BloodPressure = "Blutdruck",
-        RespiratoryRate = "Atemfrequenz"
+        Temperature = new() { Normal = "Temperatur", Low = "Hypothermie",  High = "Hyperthermie" },
+        PulseRate = new() { Normal = "Pulsfrequenz", Low = "Bradykardie", High = "Tachykardie" },
+        OxygenSaturation = new() { Normal = "Sauerstoffsättigung", Low = "Hypoxämie", High = "Hyperoxie" },
+        BloodSugar = new() { Normal = "Blutzucker", Low = "Hypoglykämie", High = "Hyperglykämie" },
+        BloodPressure = new() { Normal = "Blutdruck", Low = "Hypotonie", High = "Hypertonie" },
+        RespiratoryRate = new() { Normal = "Atemfrequenz", Low = "Bradypnoe", High = "Tachypnoe" }
     };
+    private readonly float warningToleranceValue = 0.015f; //1.5%
 
     private static bool IsGreaterThan(float a, float? b, float toleranceValue = 0.00001f)
     {
@@ -99,27 +131,67 @@ public class Checker (ICheckerDisplay display)
         return false;
     }
 
-    public string TranslateAlertMsg(string msg)
+    private static bool IsWithinLowerTolerance(float value, float? lowerTolerance, float currentToleranceValue, float toleranceValue = 0.00001f)
     {
+        if(lowerTolerance.HasValue)
+            return ((lowerTolerance+currentToleranceValue) - value) > toleranceValue;
+        return false;
+    }
+
+    private static bool IsWithinUpperTolerance(float value, float? upperTolerance, float currentToleranceValue, float toleranceValue = 0.00001f)
+    {
+        if(upperTolerance.HasValue)
+            return (value - (upperTolerance-currentToleranceValue)) > toleranceValue;
+        return false;
+    }
+
+    public string TranslateAlertMsg(PropertyInfo languagePropertyInfo, PropertyInfo levelPropertyInfo, string appendResult = "")
+    {
+        string alertMsg = string.Empty;
         switch (language)
         {
             case "English":
-                msg = $"{msg} is out of range";
+                appendResult += !appendResult.Equals(string.Empty) ? " " : "";
+                alertMsg = $"{appendResult}{levelPropertyInfo.GetValue(languagePropertyInfo.GetValue(new VitalLanguage()))}";
                 break;
             case "German":
-                msg = $"{new VitalLanguage().GetType().GetProperty(msg).GetValue(german)} ist außerhalb des Bereichs";
+                appendResult = appendResult.Equals("Near") ? "Fast " : "";
+                appendResult = appendResult.Equals("Normal") ? "Normal " : "";
+                alertMsg = $"{appendResult}{levelPropertyInfo.GetValue(languagePropertyInfo.GetValue(german))}";
                 break;
         }
-        return msg;
+        return alertMsg;
     }
 
-    public bool AlertNotInRange(string alertMsg, float reading, float? lowerLimit, float? upperLimit)
+    public bool AlertNotInRange(string propertyName, float reading, float? lowerLimit, float? upperLimit)
     {
-        alertMsg = TranslateAlertMsg(alertMsg);
-        if (IsGreaterThan(reading, upperLimit) || IsLesserThan(reading, lowerLimit))
+        float currentWarningTolerance = 0;
+        if (upperLimit != null)
+            currentWarningTolerance = (float)upperLimit * warningToleranceValue;
+        PropertyInfo languagePropertyInfo = new VitalLanguage().GetType().GetProperty(propertyName)!;
+        PropertyInfo lowLevelPropertyInfo = new VitalLevels().GetType().GetProperty("Low")!;
+        PropertyInfo highLevelPropertyInfo = new VitalLevels().GetType().GetProperty("High")!;
+        PropertyInfo normalLevelPropertyInfo = new VitalLevels().GetType().GetProperty("Normal")!;
+        if (IsGreaterThan(reading, upperLimit))
         {
-            _display.DisplayAlert(alertMsg);
+            _display.DisplayAlert(TranslateAlertMsg(languagePropertyInfo,highLevelPropertyInfo));
             return false;
+        }
+        else if (IsLesserThan(reading, lowerLimit))
+        {
+            _display.DisplayAlert(TranslateAlertMsg(languagePropertyInfo, lowLevelPropertyInfo));
+            return false;
+        }
+        else if (IsWithinLowerTolerance(reading, lowerLimit, currentWarningTolerance)){
+            _display.DisplayAlert(TranslateAlertMsg(languagePropertyInfo, lowLevelPropertyInfo,"Near"));
+        }
+        else if (IsWithinUpperTolerance(reading, upperLimit, currentWarningTolerance))
+        {
+            _display.DisplayAlert(TranslateAlertMsg(languagePropertyInfo, highLevelPropertyInfo, "Near"));
+        }
+        else
+        {
+            _display.DisplayAlert(TranslateAlertMsg(languagePropertyInfo, normalLevelPropertyInfo, "Normal"));
         }
         return true;
     }
@@ -131,7 +203,7 @@ public class Checker (ICheckerDisplay display)
                && AlertNotInRange("OxygenSaturation", spo2, 90, null);
     }
 
-    private List<PropertyInfo> GetAllProperties()
+    private static List<PropertyInfo> GetAllProperties()
     {
         return [..new Vitals().GetType().GetProperties()];
     }
--- checkertest/test.cs
+++ checkertest/test.cs
@@ -7,13 +7,15 @@ using Xunit;
 public class CheckerTests
 {
     private Mock<ICheckerDisplay> _mockDisplay = new();
-    private Checker? _checker;
+    private Checker _checker = new(new ConsoleDisplay());
     private string _language = "English";
     private Checker InitMockDisplay()
     {
         _mockDisplay = new Mock<ICheckerDisplay>();
-        _checker = new Checker(_mockDisplay.Object);
-        _checker.language = _language;
+        _checker = new Checker(_mockDisplay.Object)
+        {
+            language = _language
+        };
         return _checker;
     }
 
@@ -31,22 +33,22 @@ public class CheckerTests
     public void NotOkWhenAnyVitalIsOffRange()
     {
         Assert.True(InitMockDisplay().VitalsOk(98.1f, 70, 98)); //All okay
-        VerifyMockDisplayWithAny(Times.Never());
+        VerifyMockDisplayWithAny(Times.Exactly(3));
 
         Assert.False(InitMockDisplay().VitalsOk(94f, 75, 100)); //Temp low limit
-        VerifyMockDisplayWithMsg(Times.Once(), "Temperature is out of range");
+        VerifyMockDisplayWithMsg(Times.Once(), "Hypothermia");
 
         Assert.False(InitMockDisplay().VitalsOk(104f, 75, 100)); //Temp high limit
-        VerifyMockDisplayWithMsg(Times.Once(), "Temperature is out of range");
+        VerifyMockDisplayWithMsg(Times.Once(), "Hyperthermia");
 
         Assert.False(InitMockDisplay().VitalsOk(98f, 59, 100)); //Pulse low limit
-        VerifyMockDisplayWithMsg(Times.Once(), "PulseRate is out of range");
+        VerifyMockDisplayWithMsg(Times.Once(), "Bradycardia");
 
         Assert.False(InitMockDisplay().VitalsOk(98f, 103, 100)); //Pulse high limit
-        VerifyMockDisplayWithMsg(Times.Once(), "PulseRate is out of range");
+        VerifyMockDisplayWithMsg(Times.Once(), "Tachycardia");
 
         Assert.False(InitMockDisplay().VitalsOk(98f, 75, 89)); //SPO2 low limit
-        VerifyMockDisplayWithMsg(Times.Once(), "OxygenSaturation is out of range");
+        VerifyMockDisplayWithMsg(Times.Once(), "Hypoxemia");
 
         Assert.False(InitMockDisplay().VitalsOk(94.5f, 102, 100)); //Pulse & Temp out of range
         VerifyMockDisplayWithAny(Times.Once());
@@ -61,17 +63,16 @@ public class CheckerTests
         VerifyMockDisplayWithAny(Times.Once());
 
         Assert.True(InitMockDisplay().AlertNotInRange("Temperature", 98, 95, 102)); // Normal temperature
-        VerifyMockDisplayWithAny(Times.Never());
+        VerifyMockDisplayWithMsg(Times.Once(),"Normal Temperature");
 
         Assert.False(InitMockDisplay().AlertNotInRange("Temperature", 103, 95, 102)); //Temperature out of range
-        VerifyMockDisplayWithMsg(Times.Once(), "Temperature is out of range");
+        VerifyMockDisplayWithMsg(Times.Once(), "Hyperthermia");
 
-        Assert.True(InitMockDisplay().AlertNotInRange("Temperature", 98, null, 102)); //No temperature lower limit to test IsGreaterThan()
-        VerifyMockDisplayWithAny(Times.Never());
+        Assert.True(_checker.AlertNotInRange("Temperature", 98, null, 102)); //No temperature lower limit to test IsGreaterThan()
         
         Vitals vitals = new()
         {
-            Temperature = 98.1f,
+            Temperature = "98.1f",
             PulseRate = 70,
             OxygenSaturation = 98,
             BloodSugar = 100,
@@ -79,20 +80,25 @@ public class CheckerTests
             RespiratoryRate = 15
         };
         Assert.True(InitMockDisplay().CheckAllVitals(vitals)); //All okay
-        VerifyMockDisplayWithAny(Times.Never());
+        VerifyMockDisplayWithAny(Times.Exactly(6));
+
+        vitals.Temperature = "37c";
+        Assert.True(InitMockDisplay().CheckAllVitals(vitals)); //All okay. Temp in Celsius
+        VerifyMockDisplayWithAny(Times.Exactly(6));
+
+        vitals.BloodSugar = 109;
+        Assert.True(InitMockDisplay().CheckAllVitals(vitals)); //Blood sugar near hyper
+        VerifyMockDisplayWithMsg(Times.Exactly(1), "Near Hyperglycemia");
 
         vitals.BloodPressure = 200;
         Assert.False(InitMockDisplay().CheckAllVitals(vitals)); //Blood pressure out of range
-        VerifyMockDisplayWithMsg(Times.Once(), "BloodPressure is out of range");
+        VerifyMockDisplayWithMsg(Times.Exactly(1), "Hypertension");
 
         _language = "German";
-        Assert.False(InitMockDisplay().CheckAllVitals(vitals));
-        VerifyMockDisplayWithMsg(Times.Once(), "Blutdruck ist auﬂerhalb des Bereichs");
+        Assert.False(InitMockDisplay().CheckAllVitals(vitals)); //Blood pressure hyper
+        VerifyMockDisplayWithMsg(Times.Once(), "Hypertonie");
 
         vitals.RespiratoryRate = 50;
         Assert.False(InitMockDisplay().CheckAllVitals(vitals)); //Blood pressure and Respiratory rate out of range
-        VerifyMockDisplayWithAny(Times.Once());
-
-        //To build again
     }
 }
